00fe15d437a7289cb1f43da863f9766b405c371c
vq0599 <vq0599@outlook.com>
2018-12-26 02:25:40 +0800

    Add: object-curly-spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
     "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
     "no-else-return": [0],
     "padding-line-between-statements": [0],
+    "object-curly-spacing": ["error", "always"],
   }
 };
