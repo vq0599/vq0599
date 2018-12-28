a2bfecdc7228e3bd0697a2b7fd679d4c5ff9f0d4
vq0599 <vq0599@outlook.com>
2018-12-28 09:43:00 +0800

    pure tool file

---
+++ utils/tool.js
@@ -0,0 +1,15 @@
+export const dateTimeFormat = (ts, format = 'Y-M-D') => {
+  const date = ts instanceof Date ? ts : new Date(ts)
+  const t = {
+    Y: date.getFullYear(),
+    M: date.getMonth() + 1,
+    D: date.getDate(),
+    h: date.getHours(),
+    m: date.getMinutes(),
+    s: date.getSeconds(),
+  }
+
+  return format.replace(/([YMDhms])/g, function (key) {
+    return t[key] < 10 ? `0${t[key]}` : t[key]
+  })
+}
