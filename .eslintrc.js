70168e62a021c5505f6a2112d1310f2e733a4500
vq0599 <vq0599@outlook.com>
2019-02-16 18:37:45 +0800

    :wrench: no-unused-vars error => warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
     "no-else-return": [0],
     "padding-line-between-statements": [0],
+    "no-unused-vars": [1],
     "object-curly-spacing": ["error", "always"],
   }
 };
